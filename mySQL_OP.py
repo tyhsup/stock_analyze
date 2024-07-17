755b41cee0c39c2e1ad3fba68ccaa48b679c2f5a
ting-yu hsu <150423226+tyhsup@users.noreply.github.com>
2024-07-17 21:58:44 +0800

    Update mySQL_OP.py
    hide connection information

--- mySQL_OP.py
+++ mySQL_OP.py
@@ -6,19 +6,18 @@ import pandas as pd
 class OP_Fun:
     def cnn(self):
         connection = mysql.connector.connect(host = 'localhost',
-                                     port = '3306',
-                                     user = 'root',
-                                     password = 'terryHsup9211!',
-                                     database = 'stock_tw_analyse')
+                                     port = 'port number',
+                                     user = 'user name',
+                                     password = 'password',
+                                     database = 'database name')
         return connection
     
     def C_engine():
-        engine = create_engine('mysql+pymysql://root:terryHsup9211!@localhost:3306/stock_tw_analyse')
+        engine = create_engine('mysql+pymysql://user:password@localhost:port/database name')
         return engine
     
     def upload_all(self, data, database, table_name):
-        self.C_engine
-        data.to_sql(name = str(table_name), con = self.engine, schema = str(database), if_exists = 'append')
+        data.to_sql(name = str(table_name), con = self.C_engine, schema = str(database), if_exists = 'append')
         
     def sel_columns(self, table_name, columns_name):
         connection = self.cnn()
