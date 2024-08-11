77f7cfc35deb5613fc822b83f0cfbb0a3f9529f4
ting-yu hsu <150423226+tyhsup@users.noreply.github.com>
2024-08-11 11:33:45 +0800

    Update mySQL_OP.py
    update select cost_data function

--- mySQL_OP.py
+++ mySQL_OP.py
@@ -19,10 +19,20 @@ class OP_Fun:
     def upload_all(self, data, database, table_name):
         data.to_sql(name = str(table_name), con = self.C_engine, schema = str(database), if_exists = 'append')
         
-    def sel_columns(self, table_name, columns_name):
-        connection = self.cnn()
+    def sel_cost_data(self, table_name ='NA', columns_name = 'NA', stock_number = 'NA', *args, **kwargs):
+        connection = self.connection()
         cursor = connection.cursor()
-        cursor.execute('SELECT' + '`' + columns_name + '`' + 'FROM' + '`' + table_name + '`;')
+        print('請輸入要查詢的股票代碼')
+        stock_number = input('股票代碼:')
+        if table_name == 'NA':
+            print('no select table')
+        else :
+            if columns_name == 'NA' :
+                cursor.execute('SELECT * FROM' + '`' + table_name + '`' + 'WHERE number ='
+                               + stock_number + ';')
+            else :
+                cursor.execute('SELECT' + '`' + columns_name + '`' + 'FROM' + '`' + table_name + '`' + 'WHERE number ='
+                               + stock_number + ';')
         records = pd.DataFrame(cursor.fetchall())
         cursor.close()
         connection.close()
