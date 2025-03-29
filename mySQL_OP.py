824212ad7fadd63ce6d90f3279386a476b9d0e93
ting-yu hsu <150423226+tyhsup@users.noreply.github.com>
2025-03-29 19:40:09 +0800

    Update mySQL_OP.py
    revise code & add delete function

--- mySQL_OP.py
+++ mySQL_OP.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 class OP_Fun:
+    #設定connection 物件
     def connection(self):
         connection = mysql.connector.connect(host = '',
                                      port = '',
@@ -11,28 +12,38 @@ class OP_Fun:
                                      password = '',
                                      database = '')
         return connection
-    
+    #初始化engine
     def C_engine(self):
         engine = create_engine('')
         return engine
-    
+    #上傳資料
     def upload_all(self, data, database, table_name):
         data.to_sql(name = str(table_name), con = self.C_engine(), schema = str(database), if_exists = 'append', index = False)
-        
+    #手動輸入要從mySQL 資料庫提取的資料
     def sel_cost_data(self, table_name ='NA', columns_name = 'NA', stock_number = 'NA', *args, **kwargs):
         connection = self.connection()
         cursor = connection.cursor()
-        print('請輸入要分析的股票代碼')
+        print('請輸入SQL table')
+        table_name = input('SQL table:')
+        print('請輸入要查看的股票代碼')
         stock_number = input('股票代碼:')
         if table_name == 'NA':
-            print('no select table')
+            print('please key in table name')
         else :
-            if columns_name == 'NA' :
-                cursor.execute('SELECT * FROM' + '`' + table_name + '`' + 'WHERE number ='
-                               + stock_number + ';')
+            if columns_name == 'NA' and stock_number == 'NA' :
+                fun = f'SELECT * FROM {table_name} ;'
+                cursor.execute(fun)
+            elif columns_name == 'NA':
+                fun = f'SELECT * FROM {table_name} WHERE number = %s;'
+                number = (stock_number,)
+                cursor.execute(fun, number)
+            elif stock_number == 'NA' :
+                fun = f'SELECT {columns_name} FROM {table_name} ;'
+                cursor.execute(fun)
             else :
-                cursor.execute('SELECT' + '`' + columns_name + '`' + 'FROM' + '`' + table_name + '`' + 'WHERE number ='
-                               + stock_number + ';')
+                fun = f'SELECT {columns_name} FROM {table_name} WHERE number = %s;'
+                number = (stock_number,)
+                cursor.execute(fun, number)
         cols = cursor.description
         col = []
         for i in cols:
@@ -42,7 +53,7 @@ class OP_Fun:
         cursor.close()
         connection.close()
         return records
-    
+    #從mySQL資料庫提取資料
     def get_cost_data(self, table_name ='NA', columns_name = 'NA', stock_number = 'NA', *args, **kwargs):
         connection = self.connection()
         cursor = connection.cursor()
@@ -50,24 +61,38 @@ class OP_Fun:
             print('please key in table name')
         else :
             if columns_name == 'NA' and stock_number == 'NA' :
-                cursor.execute('SELECT * FROM' + '`' + table_name + '`;')
+                fun = f'SELECT * FROM {table_name} ;'
+                cursor.execute(fun)
             elif columns_name == 'NA':
-                cursor.execute('SELECT * FROM' + '`' + table_name + '`' + 'WHERE number ='
-                            + "'" + stock_number + "'" + ';')
+                fun = f'SELECT * FROM {table_name} WHERE number = %s;'
+                number = (stock_number,)
+                cursor.execute(fun, number)
             elif stock_number == 'NA' :
-                cursor.execute('SELECT' + '`' + columns_name + '`' + 'FROM' + '`' + table_name + '`' + ';')
+                fun = f'SELECT {columns_name} FROM {table_name} ;'
+                cursor.execute(fun)
             else :
-                cursor.execute('SELECT' + '`' + columns_name + '`' + 'FROM' + '`' + table_name + '`' + 'WHERE number ='
-                               + "'" + stock_number + "'" + ';')
+                fun = f'SELECT {columns_name} FROM {table_name} WHERE number = %s;'
+                number = (stock_number,)
+                cursor.execute(fun, number)
         cols = cursor.description
         col = []
         for i in cols:
             col.append(i[0])
         records = pd.DataFrame(cursor.fetchall(),columns = col)
-        #records.drop(records.columns[[-1]], axis =1, inplace = True)
         cursor.close()
         connection.close()
         return records
+
+    #刪除無法download的股票代碼
+    def delete_NaN_number(self, table_name ='stock_table_tw', columns_name = '有價證卷代號', stock_number = 'NA', *args, **kwargs):
+        connection = self.connection()
+        cursor = connection.cursor()
+        delete_fun = f'DELETE FROM {table_name} WHERE {columns_name} = %s ;'
+        number = (stock_number,)
+        cursor.execute(delete_fun,number)
+        connection.commit()
+        cursor.close()
+        connection.close()
 
         
         
